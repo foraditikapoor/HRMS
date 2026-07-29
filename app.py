a04fe44f7f7bb73227e0eb0df71bbb33db5d3a1c
foraditikapoor <foraditikapoor@gmail.com>
2026-07-29 12:22:17 +0530

    Fix admin leave request display

--- app.py
+++ app.py
@@ -3725,7 +3725,8 @@ def pending_leave_requests():
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
         {% endblock %}
-        """
+        """,
+        requests=requests_list,
     )
 
 
